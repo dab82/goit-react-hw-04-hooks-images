dcd999e9a5287dedae42d155b120f44423ec222c
Dmitriy Bondarenko <dabwork1982@gmail.com>
2022-01-30 20:54:25 +0200

    done

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,17 @@
 import { ThreeDots } from 'react-loader-spinner';
 
+const styles = {
+  spinner: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+};
+
 export const Loader = () => {
-  return <ThreeDots color={'#3f51b5'} width="100" ariaLabel="loading" />;
+  return (
+    <div style={styles.spinner}>
+      <ThreeDots color={'#3f51b5'} width="100" ariaLabel="loading" />
+    </div>
+  );
 };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,23 +1,20 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import propTypes from 'prop-types';
-
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ReactComponent as SearchIcon } from '../../icons/search.svg';
 import { IconButton } from '../Button/IconButton';
 import './Searchbar.css';
 
-export class Searchbar extends Component {
-  state = { query: '' };
+export const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
 
-  handleChange = event => {
-    this.setState({ query: event.currentTarget.value.toLowerCase().trim() });
+  const handleChange = event => {
+    setQuery(event.target.value.toLowerCase().trim());
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    const { query } = this.state;
-    const { onSubmit } = this.props;
 
     if (query === '') {
       toast.error('Enter what you want to find please');
@@ -25,35 +22,29 @@ export class Searchbar extends Component {
     }
 
     onSubmit(query);
-    this.setState({ query: '' });
+    setQuery('');
   };
 
-  render() {
-    return (
-      <header className="Searchbar">
-        <form
-          className="SearchForm"
-          onSubmit={this.handleSubmit}
-          autoComplete="off"
-        >
-          <IconButton aria-label="Search...">
-            <SearchIcon />
-          </IconButton>
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit} autoComplete="off">
+        <IconButton aria-label="Search...">
+          <SearchIcon />
+        </IconButton>
 
-          <input
-            className="SearchForm-input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos..."
-            onChange={this.handleChange}
-            value={this.state.query}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className="SearchForm-input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos..."
+          onChange={handleChange}
+          value={query}
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: propTypes.func.isRequired,
